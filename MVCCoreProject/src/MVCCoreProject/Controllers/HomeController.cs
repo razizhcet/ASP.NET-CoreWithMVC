0092d76a6ae02485e0ce36d0924e626013dbf289
Razi Ahmad <razizhcet@gmail.com>
2018-03-31 20:07:53 +0530

    first commit

---
+++ MVCCoreProject/src/MVCCoreProject/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCCoreProject.Model;
+
+namespace MVCCoreProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employee emp = new Employee()
+            {
+                FirstName = "Razi", LastName = "Ahmad"
+            };
+            return View(emp);
+        }
+    }
+}
---
+++ MVCCoreProject/src/MVCCoreProject/Model/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreProject.Model
+{
+    public class Employee
+    {
+        public String FirstName { set; get; }
+        public String LastName { set; get; }
+    }
+}
